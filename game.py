1384b353e86d9a938b4c63ffbc0f19df454e2f84
Andrew Petro <andrew.petro@wisc.edu>
2022-09-04 15:47:13 -0500

    game loop forever

--- game.py
+++ game.py
@@ -7,16 +7,17 @@ def play():
 
   inventory = ['Club', 'Load of bread']
 
-  action_input = get_player_command()
+  while True: # play until quit
+    action_input = get_player_command()
 
-  if action_input in ['n', 'N']:
-    print("You go north.")
-  elif action_input in ['s', 'N']:
-    print("You go south.")
-  elif action_input in ['i', 'I']:
-    print("Inventory: ")
-    print(inventory)
-  else:
-    print("Error: unrecognized input.")
+    if action_input in ['n', 'N']:
+      print("You go north.")
+    elif action_input in ['s', 'N']:
+      print("You go south.")
+    elif action_input in ['i', 'I']:
+      print("Inventory: ")
+      print(inventory)
+    else:
+      print("Error: unrecognized input.")
 
 play()
