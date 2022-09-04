c749cf8009fb329c33d4a9ee065e71925f7317af
Andrew Petro <andrew.petro@wisc.edu>
2022-09-04 15:32:23 -0500

    add inventory action

--- game.py
+++ game.py
@@ -4,12 +4,18 @@ def get_player_command():
   return input("Action: ")
 
 def play():
+
+  inventory = ['Club', 'Load of bread']
+
   action_input = get_player_command()
 
   if action_input == 'n':
     print("You go north.")
   elif action_input == 's':
     print("You go south.")
+  elif action_input == 'i':
+    print("Inventory: ")
+    print(inventory)
   else:
     print("Error: unrecognized input.")
 
