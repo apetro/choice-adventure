4dbfb12f9a02e2a9e5bfa4b85412a5f28b187c61
Andrew Petro <andrew.petro@hey.com>
2022-09-06 10:32:37 -0500

    add Weapon classes

---
+++ docs/log/007_objects.md
@@ -0,0 +1,12 @@
+# 007 Objects
+
+Define classes with `class`.
+
+```python
+class Weapon:
+  __str__(self):
+    return self.name
+class Rock(Weapon):
+  __init__(self):
+    self.name = "Rock"
+```
--- game.py
+++ game.py
@@ -1,11 +1,33 @@
 print("Escape from the Disquieting Cavern!")
 
+class Weapon:
+  def __str__(self):
+    return self.name
+
+class Rock(Weapon):
+  def __init__(self):
+    self.name = "Rock"
+    self.description = "A fist-sized rock suitable for bludgeoning."
+    self.damage = 1
+
+class Dagger(Weapon):
+  def __init__(self):
+    self.name = "Dagger"
+    self.description = "A solid but rusty knife"
+    self.damage = 3
+
+class Club(Weapon):
+  def __init__(self):
+    self.name = "Club"
+    self.description = "A solid wooden club suitable for bludgeoning"
+    self.damage = 3
+
 def get_player_command():
   return input("Action: ")
 
 def play():
 
-  inventory = ['Club', 'Load of bread']
+  inventory = [Club(), 'Load of bread']
 
   while True: # play until quit
     action_input = get_player_command()
