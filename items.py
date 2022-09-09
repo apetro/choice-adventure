a5911ee4bcd1974ce9b6e217afab7bf9ea40f22b
Andrew Petro <andrew.petro@hey.com>
2022-09-09 16:48:07 -0500

    items have gold_value

--- items.py
+++ items.py
@@ -7,22 +7,38 @@ class Rock(Weapon):
     self.name = "Rock"
     self.description = "A fist-sized rock suitable for bludgeoning."
     self.damage = 1
+    self.gold_value = 1
 
 class Dagger(Weapon):
   def __init__(self):
     self.name = "Dagger"
     self.description = "A solid but rusty knife"
     self.damage = 3
+    self.gold_value = 3
 
 class Club(Weapon):
   def __init__(self):
     self.name = "Club"
     self.description = "A solid wooden club suitable for bludgeoning"
     self.damage = 3
+    self.gold_value = 1
 
 class Analgesic:
-  def __init__(self, name, healing_value):
+  def __init__(self, name, healing_value, gold_value):
     self.name = name
     self.healing_value = healing_value
+    self.gold_value = gold_value
   def __str__(self):
     return "{} (+{} health)".format(self.name, self.healing_value)
+
+class CrustyBread(Analgesic):
+    def __init__(self):
+        super.__init__("Crusty bread", 5, 5)
+
+class PotionOfMinorHealing(Analgesic):
+    def __init__(self):
+        super.__init__("Potion of minor healing", 10, 10)
+
+class OptionOfMajorHealing(Analgesic):
+    def __init__(self):
+        super.__init__("Potion of major healing", 20, 40)
